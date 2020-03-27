076e0ffd3fd9f99c8ecc2a2701db46361fa68765
thallesdaniel <thallesdaniell@hotmail.com>
2020-03-27 12:14:56 -0300

    correção coluna phone

--- database/migrations/2020_03_27_002946_create_phones_table.php
+++ database/migrations/2020_03_27_002946_create_phones_table.php
@@ -15,6 +15,7 @@ class CreatePhonesTable extends Migration
     {
         Schema::create('phones', function (Blueprint $table) {
             $table->id();
+            $table->string('phone');
             $table->timestamps();
             $table->softDeletes();
         });
