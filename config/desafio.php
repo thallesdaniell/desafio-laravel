4e9e6e8cb4c655554220e3e6eb8cfb12d8d53770
thallesdaniel <thallesdaniell@hotmail.com>
2020-03-27 11:18:15 -0300

    criado config para aplicação

---
+++ config/desafio.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    /*
+      |--------------------------------------------------------------------------
+      | Default role administrador
+      |--------------------------------------------------------------------------
+      |
+      | Após aplicar o seed será criado uma role administrador para poder acessar as permissions
+      | e assim editar caso precise
+      |
+     */
+
+    'role-admin'     => 'administrador',
+];
