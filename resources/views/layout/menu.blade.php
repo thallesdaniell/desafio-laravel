cf8e1a6b1f60963cb2278da53a468ee406c73ea3
thallesdaniel <thallesdaniell@hotmail.com>
2020-03-28 15:35:12 -0300

    adicionado rotas menu

---
+++ resources/views/layout/menu.blade.php
@@ -0,0 +1,28 @@
+<div class="main-sidebar">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="{{route('home')}}">DESAFIO</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="{{route('home')}}">PC</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Agenda</li>
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Agenda</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="{{route('client.index')}}">Contatos</a></li>
+                    <li><a class="nav-link" href="{{route('user.edit',auth()->user()->id)}}">Perfil</a></li>
+                </ul>
+            </li>
+            <li class="menu-header">Administrador</li>
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Administrador</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="{{route('user.index')}}">Usuários</a></li>
+                    <li><a class="nav-link" href="{{route('role.index')}}">Gerenciar Perfil</a></li>
+                </ul>
+            </li>
+        </ul>
+    </aside>
+</div>
