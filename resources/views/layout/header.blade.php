fc8d43e98bb6536adbfcf426eadd3479f9620965
thallesdaniel <thallesdaniell@hotmail.com>
2020-03-28 17:32:05 -0300

    ajuste path dos assets

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -8,7 +8,7 @@
     </form>
     <ul class="navbar-nav navbar-right">
         <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-                <img alt="image" src="../assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
+                <img alt="image" src="{{asset('assets/img/avatar/avatar-1.png')}}" class="rounded-circle mr-1">
                 <div class="d-sm-none d-lg-inline-block">{{auth()->user()->name}}</div></a>
             <div class="dropdown-menu dropdown-menu-right">
                 <a href="{{route('user.edit',auth()->user()->id)}}" class="dropdown-item has-icon">
--- resources/views/layout/styles.blade.php
+++ resources/views/layout/styles.blade.php
@@ -7,7 +7,7 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
 <!-- Template CSS -->
-<link rel="stylesheet" href="../assets/css/style.css">
-<link rel="stylesheet" href="../assets/css/components.css">
+<link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/components.css')}}">
 
-<link rel="shortcut icon" href="../assets/img/stisla.svg">
+<link rel="shortcut icon" href="{{asset('assets/img/stisla.svg')}}">
