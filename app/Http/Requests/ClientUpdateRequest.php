482b13890b6f6b60b5da094e1e142f304ed13d19
thallesdaniel <thallesdaniell@hotmail.com>
2020-03-29 16:09:08 -0300

    criado requests para clientes

---
+++ app/Http/Requests/ClientUpdateRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class ClientUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|email',
+            'name'  => 'required|string',
+            'phone' => Rule::requiredIf(function () {
+                $return = false;
+                foreach ($this->phones as $phones) {
+                    $sum    = preg_replace('/[^0-9]/', '', $phones);
+                    if (strlen($sum) == 11) continue;
+                    $return = true;
+                }
+                return $return;
+            }),
+        ];
+    }
+
+    /**
+     * Get custom messages for validator errors.
+     *
+     * @return array
+     */
+    public function messages() {
+
+        return [
+            'phone.required' => 'Telefone informado não é válido!'
+        ];
+    }
+}
