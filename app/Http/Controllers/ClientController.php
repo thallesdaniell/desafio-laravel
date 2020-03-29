6f6a4d43d357f58d60d60cd7977fe8056a234b56
thallesdaniel <thallesdaniell@hotmail.com>
2020-03-29 16:17:17 -0300

    criado controller para cliente

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ClientStoreRequest;
+use App\Http\Requests\ClientUpdateRequest;
+use App\Models\Client;
+
+class ClientController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('permission:cliente-listar', ['only' => ['index', 'show']]);
+        $this->middleware('permission:cliente-criar', ['only' => ['create', 'store']]);
+        $this->middleware('permission:cliente-editar', ['only' => ['edit', 'update']]);
+        $this->middleware('permission:cliente-deletar', ['only' => ['destroy']]);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $clients = Client::where('user_id', auth()->user()->id)->orderBy('name', 'ASC')->get();
+        return view('client.index', compact('clients'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('client.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ClientStoreRequest $request)
+    {
+        $client          = new Client();
+        $client->user_id = auth()->user()->id;
+        $client->name    = $request->get('name');
+        $client->email   = $request->get('email');
+        $client->save();
+
+        foreach ($request->get('phones') as $phone)
+            $phones[] = ['phone' => $phone];
+
+        $client->phone()->delete();
+        $client->phone()->createMany($phones);
+
+        return redirect()->route('client.index')
+            ->with('message', 'Cliente adicionado com sucesso.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+       //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $client = Client::findOrFail($id);
+
+        $this->authorize('permission', $client);
+
+        return view('client.edit', compact('client'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ClientUpdateRequest $request, $id)
+    {
+        $client        = Client::findOrFail($id);
+        $client->name  = $request->get('name');
+        $client->email = $request->get('email');
+
+        $this->authorize('permission', $client);
+
+        $client->save();
+
+        foreach ($request->get('phones') as $phone)
+            $phones[] = ['phone' => $phone];
+
+        $client->phone()->delete();
+        $client->phone()->createMany($phones);
+
+        return redirect()->route('client.index')
+            ->with('message', 'Cliente editado com sucesso.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $client = Client::findOrFail($id);
+
+        $this->authorize('permission', $client);
+
+        $client->delete();
+        return redirect()->route('client.index')
+            ->with('message', 'Cliente deletado com sucesso.');
+    }
+}
