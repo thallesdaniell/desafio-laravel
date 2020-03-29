f9942f0911bbcf49caa224e0088d132b592727fc
thallesdaniel <thallesdaniell@hotmail.com>
2020-03-28 21:59:52 -0300

    criado policy para usuário editar apenas suas informações

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function permission(User $userAuth, User $user)
+    {
+        return $userAuth->id === $user->id;
+    }
+}
