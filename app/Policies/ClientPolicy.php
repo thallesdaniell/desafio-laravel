9ed730483ef616c0cdefc54ba65792dd6be01475
thallesdaniel <thallesdaniell@hotmail.com>
2020-03-29 16:14:00 -0300

    criado policy para cliente

---
+++ app/Policies/ClientPolicy.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Client;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ClientPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function permission(User $user, Client $client)
+    {
+        return $user->id === $client->user_id;
+
+    }
+}
