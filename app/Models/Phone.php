2eccfd8c8785b029c6962e90a7703ce98a5e20a6
thallesdaniel <thallesdaniell@hotmail.com>
2020-03-28 21:40:59 -0300

    criado model phone

---
+++ app/Models/Phone.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Phone extends Model
+{
+    use SoftDeletes;
+
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function setPhoneAttribute($value)
+    {
+        $this->attributes['telefone'] = str_replace(['(',')','-'],[''],$value);
+    }
+
+    public function getPhoneAttribute($value)
+    {
+        return $this->mask('(##) #####-####',$value);
+    }
+
+    private function mask($mask, $value)
+    {
+        $lengthMask   = substr_count($mask, '#');
+        $lengthString = strlen($value);
+        if ($lengthMask == $lengthString) {
+            $value = str_replace(' ', '', $value);
+            for ($i = 0; $i < $lengthString; $i++)
+                $mask[strpos($mask, '#')] = $value[$i];
+            return $mask;
+        }
+        return $value;
+    }
+
+
+}
