2ec292c14c3597b4ebf5c6968d55dd6e26536bfc
thallesdaniel <thallesdaniell@hotmail.com>
2020-04-02 13:31:59 -0300

    novas configurações de perfis

--- config/desafio.php
+++ config/desafio.php
@@ -6,11 +6,31 @@ return [
       | Default role administrador
       |--------------------------------------------------------------------------
       |
-      | Após aplicar o seed será criado uma role administrador para poder acessar as permissions
-      | e assim editar caso precise
+      | Role administrador para poder acessar as permissions
+      | acesso geral do sistema
       |
      */
 
-    'role-admin'   => 'administrador',
-    'role-default' => 'padrao',
+    'role-admin'   => "Administrador",
+    /*
+      |--------------------------------------------------------------------------
+      | Default role princiapl
+      |--------------------------------------------------------------------------
+      |
+      | Role para conta principal do usuário onde poderá adicionar convidados
+      | e configurar perfis
+      |
+     */
+    'role-default' => "Conta Principal",
+
+    /*
+      |--------------------------------------------------------------------------
+      | Default role convidado
+      |--------------------------------------------------------------------------
+      |
+      | Role para acessar com restrições
+      |
+      |
+     */
+    'role-guest'   => "Conta Convidado"
 ];
