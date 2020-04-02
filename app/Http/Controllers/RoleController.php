b0de8c4809fa50719c9e55628063660cdeccb53e
thallesdaniel <thallesdaniell@hotmail.com>
2020-04-02 13:44:23 -0300

    permitir editar apenas roles criadas pelo user auth

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
@@ -10,7 +11,7 @@ class RoleController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('role:'.config('desafio.role-admin'));
+        $this->middleware('role:' . config('desafio.role-default'));
     }
 
     /**
@@ -20,7 +21,10 @@ class RoleController extends Controller
      */
     public function index()
     {
-        $roles = Role::all();
+        $roles = Auth::user()->roles
+            ->whereNotIn('name', [
+                config('desafio.role-default'), config('desafio.role-guest')
+            ]);
         return view('role.index', compact('roles'));
     }
 
@@ -38,7 +42,7 @@ class RoleController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -56,6 +60,7 @@ class RoleController extends Controller
         $permissions = $request['permissions'];
 
         $role->save();
+        Auth::user()->assignRole($name);
 
         foreach ($permissions as $permission) {
             $p = Permission::where('id', '=', $permission)->firstOrFail();
@@ -71,7 +76,7 @@ class RoleController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -82,13 +87,18 @@ class RoleController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
         $permissions      = Permission::all();
-        $role             = Role::findOrFail($id);
+        $role             = Auth::user()
+            ->roles()
+            ->whereNotIn('name', [
+                config('desafio.role-default'), config('desafio.role-guest')
+            ])
+            ->findOrFail($id);
         $role_permissions = $role->permissions->pluck('name');
 
         return view('role.edit', compact('role', 'permissions', 'role_permissions'));
@@ -97,8 +107,8 @@ class RoleController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -134,17 +144,12 @@ class RoleController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
         $role = Role::findOrFail($id);
-
-        if (count($role->users)) {
-            return redirect()->route('role.index')
-                ->with('message_error', 'Atenção! Existem usuários atribuídos a esse perfil. Antes de excluir o perfil, revise estes usuários.');
-        }
 
         $role->delete();
         return redirect()->route('role.index')
