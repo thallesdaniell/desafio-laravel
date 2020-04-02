179b4dcfcd688142637ba046e13f1ca09bffc627
thallesdaniel <thallesdaniell@hotmail.com>
2020-04-02 16:59:02 -0300

    inserido checagem de regras para acessar os menus

--- resources/views/layout/menu.blade.php
+++ resources/views/layout/menu.blade.php
@@ -7,29 +7,34 @@
             <a href="{{route('home')}}">PC</a>
         </div>
         <ul class="sidebar-menu">
-            <li class="menu-header">Agenda</li>
+            <li class="menu-header">Menu</li>
             <li class="nav-item dropdown">
                 <a href="#" class="nav-link has-dropdown"><i class="far fa-address-book"></i><span>Agenda</span></a>
                 <ul class="dropdown-menu">
-                    <li><a class="nav-link" href="{{route('client.index')}}">Clientes</a></li>
+                    @canany([config("role-default"),"Visualizar Telefones","Editar Telefone","Excluir Telefone"])
+                        <li><a class="nav-link" href="{{route('client.index')}}">Clientes</a></li>
+                    @endcanany
+                    @can(config("role-default"))
+                        <li><a class="nav-link" href="{{route('user.index')}}">Usuários</a></li>
+                    @endcan
+                    @can(config("role-default"))
+                        <li><a class="nav-link" href="{{route('role.index')}}">Perfis</a></li>
+                    @endcan
+                    @canany([config("role-default"),"Visualizar Histórico","Visualizar Histórico Todos"])
+                        <li><a class="nav-link" href="{{route('log.index')}}">Histórico</a></li>
+                    @endcanany
                 </ul>
             </li>
 
-            <li class="menu-header">Histórico</li>
-            <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="far fa-address-book"></i><span>Histórico</span></a>
-                <ul class="dropdown-menu">
-                    <li><a class="nav-link" href="{{route('log.index')}}">Histórico</a></li>
-                </ul>
-            </li>
 
             @can(config('desafio.role-admin'))
             <li class="menu-header">Administrador</li>
             <li class="nav-item dropdown">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-users-cog"></i> <span>Administrador</span></a>
                 <ul class="dropdown-menu">
-                    <li><a class="nav-link" href="{{route('user.index')}}">Usuários</a></li>
-                    <li><a class="nav-link" href="{{route('role.index')}}">Perfis</a></li>
+                    <li><a class="nav-link" href="{{route('admin-user.index')}}">Usuários</a></li>
+                    <li><a class="nav-link" href="{{route('admin-role.index')}}">Perfis</a></li>
+                    <li><a class="nav-link" href="{{route('log.index')}}">Histórico</a></li>
                 </ul>
             </li>
             @endcan
